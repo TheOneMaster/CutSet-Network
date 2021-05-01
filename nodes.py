9d1c04b9842f2d17ed56969c8b30cb909d031cee
nayan <TheOneMaster@users.noreply.github.com>
2021-05-02 01:30:20 +0200

    Node now stores depth in tree

--- nodes.py
+++ nodes.py
@@ -1,41 +1,39 @@
 import numpy as np
 from collections import namedtuple
 
-from numpy.core.fromnumeric import var
-
 Name = namedtuple('Node', ['variable', 'split', 'l_prob', 'r_prob', 'data', 'inf_gain'])
 
 
 class Node:
 
-    def __init__(self, left_prob=0, right_prob=0, variable=0, split_value=0, data_points=0, information_gain=0) -> None:
+    def __init__(self, left_prob=0, right_prob=0, variable=0, split_value=0, data_points=0, information_gain=0, depth=0) -> None:
         
         self.left_prob = left_prob
         self.right_prob = right_prob
-        self.left_child = None
-        self.right_child = None
-
         self.variable = variable
         self.split_value = split_value
         self.data_points = data_points
         self.information_gain = information_gain
+        self.depth = depth
 
+        self.left_child = None
+        self.right_child = None
     def __str__(self) -> str:
         n = Name(self.variable, self.split_value, self.left_prob, self.right_prob, self.data_points, self.information_gain)
         return str(n)
 
-    def printTree(self, depth=0) -> None:
+    def printTree(self) -> None:
         
-        print(f"{depth*'-'}[{self.variable} < {self.split_value}] [{self.data_points}]")
+        print(f"{self.depth*'-'}[{self.variable} < {self.split_value}] [{self.data_points}]")
 
         children = [self.left_child, self.right_child]
 
         for child in children:
 
             if isinstance(child, Node):
-                child.printTree(depth=depth+1)
+                child.printTree()
             else:
-                print(f"{depth*'-'} [Chow-Liu Tree]")
+                print(f"{self.depth+1*'-'} [Chow-Liu Tree]")
         
     def findLeaf(self, row: np.ndarray) -> str:
 
